fe494b73500330d4d4683495df133be7deb5b7a9
adilahamad786 <adilahamad.personally@gmail.com>
2022-09-28 17:55:38 +0530

    Basic Meals section completed.

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealeSummary";
+
+const Meals = () => {
+    return (
+        <>
+            <MealsSummary />
+            <AvailableMeals />
+        </>
+    );
+}
+
+export default Meals;
