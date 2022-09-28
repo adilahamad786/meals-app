5eeac9b385ab938428e7e4f98017d4738639e118
adilahamad786 <adilahamad.personally@gmail.com>
2022-09-28 17:09:14 +0530

    Complete all header related components.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import HeaderCartButton from "./HeaderCartButton";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg"
+
+const Header = () => {
+    return (
+        <>
+            <header className={ classes.header}>
+                <h1>Meals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt="A table full of delicious meals!" />
+            </div>
+        </>
+    );
+}
+
+export default Header;
