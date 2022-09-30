d4917c00846744dc981e0bebc573bc4cdcea7270
adilahamad786 <adilahamad.personally@gmail.com>
2022-09-30 17:34:54 +0530

    Cart Logic implemented using props drill.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+
+  const [cartIsShown, setCardIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCardIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCardIsShown(false);
+  }
+
   return (
     <>
-      <Cart />
-      <Header />
+      { cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,12 @@ import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg"
 
-const Header = () => {
+const Header = (props) => {
     return (
         <>
             <header className={ classes.header}>
                 <h1>Meals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onShowCart={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="A table full of delicious meals!" />
